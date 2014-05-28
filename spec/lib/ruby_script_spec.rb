debdf9b9b72d8dcb001f69d3d7825a9c50f3cb43
Vinicius Ferreira Negrisolo <vinicius.negrisolo@gmail.com>
2014-05-27 23:13:47 -0300

    remove the config spec because of class config instance

--- spec/lib/ruby_script_spec.rb
+++ spec/lib/ruby_script_spec.rb
@@ -2,12 +2,6 @@ require 'spec_helper'
 
 describe RubyScript do
 
-  describe '#config' do
-    it 'log_datetime default' do
-      expect(RubyScript.config.log_datetime).to be true
-    end
-  end
-
   describe '#configure' do
     it 'log_datetime set to true' do
       RubyScript.configure{ |conf| conf.log_datetime = true }
